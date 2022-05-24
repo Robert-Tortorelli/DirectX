3a7d2ea54f62b89a626baab836aa740fbd234648
Robert-Tortorelli <65745002+Robert-Tortorelli@users.noreply.github.com>
2022-05-24 17:34:10 -0400

    Added the version number (3.0) to the comments. Added the text of the associated GitHub repository's README.md to the comments.

--- objRenderer/objRenderer.cpp
+++ objRenderer/objRenderer.cpp
@@ -1,8 +1,11 @@
 ﻿// objRenderer
-// objRenderer uses its objReader function to read and parse a single 3D object's description from a Wavefront .obj file, for subsequent rendering using DirectX.
-//
-// Author: This program, except for its objReader function, is primarily based on http://DirectXTutorial.com's "DirectX 11 Win32 Desktop: Direct3D: Moving to 3D: Lesson 3: Simple Modeling" and earlier lessons, by Chris Hanson.
-//		   The objReader function was written by Robert John Tortorelli.
+// Version 3.0
+// 
+// README
+// This project consists of the program objRenderer, which parses a single 3D object's description from a Wavefront .obj file and renders the object.
+// This is a C++ Windows Desktop application using the Windows (Win32) API and the DirectX 11 API.
+// Authorship: This program, except for the objReader function that parses the Wavefront.obj file, is based on "DirectX 11 Win32 Desktop: Direct3D: Moving to 3D: Lesson 3: Simple Modeling" and earlier lessons by Chris Hanson(https ://DirectXTutorial.com).
+// All defects in this program are soley the responsibility of Robert John Tortorelli.
 
 // Global Declarations: Start
 
