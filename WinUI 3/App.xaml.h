7cfcfd0f7151a77f2ebad8c4b6e5e9caad0f0e78
Robert-Tortorelli <65745002+Robert-Tortorelli@users.noreply.github.com>
2022-06-13 15:07:26 -0400

    WinUI 3 sample app using the 'Blank App, Packaged (WinUI 3 in Desktop)' project template

---
+++ WinUI 3/App.xaml.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::WinUI_3::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
